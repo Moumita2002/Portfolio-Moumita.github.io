64ecca15c788bc784e837e33fb8d4fe3f3e21303
Moumita2002 <102172188+Moumita2002@users.noreply.github.com>
2023-02-26 01:34:29 +0530

    Portfolio

---
+++ index.js
@@ -0,0 +1,51 @@
+let Education=document.getElementById('Education');
+let TechnicalSkills=document.getElementById('TechnicalSkills');
+let SoftSkills=document.getElementById('SoftSkills');
+let Achievements=document.getElementById('Achievements');
+
+function Edu(){
+
+    Education.style.display="block";
+    TechnicalSkills.style.display="none";
+    SoftSkills.style.display="none"; 
+    Achievements.style.display="none";
+}
+
+function Tech(){
+
+    Education.style.display="none";
+    TechnicalSkills.style.display="block";
+    SoftSkills.style.display="none"; 
+    Achievements.style.display="none";
+    
+}
+
+function Soft(){
+
+    Education.style.display="none";
+    TechnicalSkills.style.display="none";
+    SoftSkills.style.display="block"; 
+    Achievements.style.display="none";
+
+}
+
+function Achieve(){
+    Education.style.display="none";
+    TechnicalSkills.style.display="none";
+    SoftSkills.style.display="none"; 
+    Achievements.style.display="block";
+}
+
+let next=document.getElementById('next');
+let other=document.getElementById('other');
+let other1=document.getElementById('other1');
+
+function nextButton(){
+    if(other.display='none')
+    {
+        other.style.display='block';
+        other1.style.display='none';
+    }
+    else
+    other.style.display='none';
+}
